3e42e38095541deed73546b449c848a75cf98dd7
yday05506 <yangdayeon2@gmail.com>
2023-08-08 11:08:35 +0900

    GUI 디자인 작업

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,20 @@ namespace LinqQUIApp
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
