a63bfcc02c53e9607b2e20818ce46c863a103e51
yday05506 <yangdayeon2@gmail.com>
2023-08-08 11:41:39 +0900

    Linq를 사용해 버튼이 동작하도록 로직 구현

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -53,6 +53,7 @@
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.Size = new System.Drawing.Size(486, 426);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,18 @@ namespace LinqQUIApp
 {
     public partial class Form1 : Form
     {
+        static private List<Product> products = new List<Product>()
+        {
+            new Product() {Name = "맥북", Price=2000000},
+            new Product() {Name = "메모장", Price=2000},
+            new Product() {Name = "샤프", Price=3000},
+            new Product() {Name = "노트", Price=3000},
+            new Product() {Name = "웹캠", Price=200000},
+            new Product() {Name = "스마트폰", Price=2100000},
+            new Product() {Name = "가방", Price=50000},
+            new Product() {Name = "껌", Price=1000},
+            new Product() {Name = "가방", Price=50000},
+        };
         public Form1()
         {
             InitializeComponent();
@@ -19,15 +31,25 @@ namespace LinqQUIApp
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            productBindingSource.DataSource = from item in products
+                                              where item.Price >= 1500
+                                              orderby item.Price descending
+                                              select item;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            productBindingSource.DataSource = from item in products
+                                              orderby item.Name ascending
+                                              select item;
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            productBindingSource.DataSource = products; 
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
